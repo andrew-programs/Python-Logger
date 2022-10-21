c9d259c178e246aa6fc6e0a4bdb3302d2c152df3
Andrew Williams <110430206+andrew-programs@users.noreply.github.com>
2022-10-21 16:28:09 -0600

    Refactored Logger.py and wrote README.md
    Went back to update code with a fresh mind and newer techniques that I've picked up since initial commit.

--- Logger.py
+++ Logger.py
@@ -1,155 +1,126 @@
-import platform
+from platform import system
 from dataclasses import dataclass
 from datetime import datetime
-import os
+from os import rename
 
 @dataclass
 class Logger:
     def __init__(self, directory: str):
-        # Note for osSystem: Darwin is MacOS on M1 Chip
-        self.osSystem = platform.system()
-        self.osRelease = platform.release()
-        self.osVersion = platform.version()
-
-        assert directory != '', 'Please fill out directory'
+        if directory.endswith('/') or directory.endswith('\\'):
+            self.directory = directory[:-1]
+        else:
+            self.directory = directory
         
-        if self.osSystem == 'Darwin':
+        osName = system()
+        
+        # Checks if OS is either MacOS, Windows, or Linux and generates a separator.
+        if osName == 'Darwin' or osName == 'Linux':
             self.separator = '/'
-        elif self.osSystem == 'Linux':
-            self.separator = '/'
-        elif self.osSystem == 'Windows':
+        elif osName == 'Windows':
             self.separator = '\\'
         else:
-            raise OSError(f'Sorry, {self.osSystem} is not supported.')
+            raise OSError(f'Sorry, {osName} is not supported.')
         
-        self.directory = directory
-        self.filePath = f'{directory}{self.separator}{datetime.now():%m-%d-%Y %H-%M-%S}.log'
+        self.filePath = f'{self.directory}{self.separator}{datetime.now():%m-%d-%Y %H-%M-%S}.log'
+        
+        # Creates log file.
         try:
             with open(f'{self.filePath}', 'x') as file:
                 pass
         except FileExistsError:
-            pass
-    
-    def renameLog(self):
-        # Will attempt to rename file as each log happens so no mismatching of names/content.
-        newPath = f'{self.directory}{self.separator}{datetime.now():%m-%d-%Y %H-%M-%S}.log'
-        
-        if self.filePath != newPath:
-            os.rename(self.filePath, newPath)
-            self.filePath = newPath
-            
+            raise FileExistsError('For some reason, this file already exists')
 
-    def log(self, message: str, importance: str=None) -> str:
-        """
-        This method allows you to append importance, timestamps, and logs to write a line to the currently active log file.
-
-        Example
-        -------
-        >>> logger = Logger(directory)
-            result = 2 + 2
-            logger.log(f'Math completed, result = {result}', 'MEDIUM')
+    # Creates a new line in current log as any importance passed into the method.
+    def log(self, message: str, importance: str=None) -> None:
+        """Creates a new line in current log as any importance passed into the method."""
         
-        Parameters
-        ----------
-        importance: str
-            Allows the user to rate importance of new line to log. This can be left out if importance means nothing to you.
-        """
-        # Will attempt to append the currently active log with messages and importance. If it fails, returns string with exception.
+        assert message != None, 'message cannot be None'
+
+        # Attempts to append the currently active log with messages and importance. If it fails, returns string with exception.
         try:
-            # Checks if importance has been provided and will append it to the message to create the line. If not given, it will not be included.
-            assert message != None, 'message cannot be None'
-            
+            # Creates a new line in current log and renames log to current datetime.
             with open(f'{self.filePath}', 'a') as file:
-                if importance is not None:
-                    file.write(f'{datetime.now()} | {importance} | {message}\n')
-                else:
+                if importance == None:
                     file.write(f'{datetime.now()} | {message}\n')
-            
-            self.renameLog()
-        except Exception:
-            pass
+                else:
+                    file.write(f'{datetime.now()} | {importance} | {message}\n')
+                self.__renameLog()
+        
+        except Exception as exception:
+            print(f'Could not print to log due to exception : {exception}')
     
-    def low(self, message: str):
-        """
-        Allows you to append timestamps and log messages and output them to the currently active log file under low importance.
-
-        Example
-        -------
-        >>> logger = Logger(directory)
-            result = 2 + 2
-            logger.low(f'Math completed, result = {result}')
-        """
+    # Creates a new line in current log as low importance.
+    def low(self, message: str) -> None:
+        """Creates a new line in current log as low importance."""
+        
+        assert message != None, 'message cannot be None'
+        
+        # Attempts to append the currently active log with messages and importance. If it fails, prints exception.
         try:
-            # Checks if importance has been provided and will append it to the message to create the line. If not given, it will not be included.
-            assert message != None, 'message cannot be None'
-            
+            # Logs message in current log and renames log to current datetime.
             with open(f'{self.filePath}', 'a') as file:
                 file.write(f'{datetime.now()} | LOW | {message}\n')
-            
-            self.renameLog()
-        except Exception:
-            pass
-        
-    def medium(self, message: str):
-        """
-        Allows you to append timestamps and log messages and output them to the currently active log file under medium importance.
+                self.__renameLog()
 
-        Example
-        -------
-        >>> logger = Logger(directory)
-            result = 2 + 2
-            logger.medium(f'Math completed, result = {result}')
-        """
+        except Exception as exception:
+            print(f'Could not print to log due to exception : {exception}')
+    
+    # Creates a new line in current log as medium importance.
+    def medium(self, message: str) -> None:
+        """Creates a new line in current log as medium importance."""
+        
+        assert message != None, 'message cannot be None'
+
+        # Attempts to append the currently active log with messages and importance. If it fails, prints exception.
         try:
-            # Checks if importance has been provided and will append it to the message to create the line. If not given, it will not be included.
-            assert message != None, 'message cannot be None'
-            
+            # Logs message in current log and renames log to current datetime
             with open(f'{self.filePath}', 'a') as file:
                 file.write(f'{datetime.now()} | MEDIUM | {message}\n')
+                self.__renameLog()
             
-            self.renameLog()
-        except Exception:
-            pass
+        except Exception as exception:
+            print(f'Could not print to log due to exception : {exception}')
     
-    def high(self, message: str):
-        """
-        Allows you to append timestamps and log messages and output them to the currently active log file under high importance.
+    # Creates a new line in current log as high importance.
+    def high(self, message: str) -> None:
+        """Creates a new line in current log as high importance."""
+        
+        assert message != None, 'message cannot be None'
 
-        Example
-        -------
-        >>> logger = Logger(directory)
-            result = 2 + 2
-            logger.high(f'Math completed, result = {result}')
-        """
+        # Attempts to append the currently active log with messages and importance. If it fails, prints exception.
         try:
-            # Checks if importance has been provided and will append it to the message to create the line. If not given, it will not be included.
-            assert message != None, 'message cannot be None'
-            
+            # Logs message in current log and renames log to current datetime
             with open(f'{self.filePath}', 'a') as file:
                 file.write(f'{datetime.now()} | HIGH | {message}\n')
-            
-            self.renameLog()
-        except Exception:
-            pass
+                self.__renameLog()
     
-    def info(self, message: str):
-        """
-        This method allows you to append importance, timestamps, and logs to write a line to the currently active log file.
+        except Exception as exception:
+            print(f'Could not print to log due to exception : {exception}')
+    
+    # Creates a new line in current log as information.
+    def info(self, message: str) -> None:
+        """Creates a new line in current log as information."""
+        
+        assert message != None, 'message cannot be None'
 
-        Example
-        -------
-        >>> logger = Logger(directory)
-            result = 2 + 2
-            logger.info(f'Math completed, result = {result}')
-        """
-        # Will attempt to append the currently active log with messages and importance. If it fails, returns string with exception.
+        # Attempts to append the currently active log with messages and importance. If it fails, returns string with exception.
         try:
-            # Checks if importance has been provided and will append it to the message to create the line. If not given, it will not be included.
-            assert message != None, 'message cannot be None'
-            
+            # Logs message in current log and renames log to current datetime
             with open(f'{self.filePath}', 'a') as file:
                 file.write(f'{datetime.now()} | INFO | {message}\n')
-       
-            self.renameLog()
-        except Exception:
-            pass
+                self.__renameLog()
+
+        except Exception as exception:
+            print(f'Could not print to log due to exception : {exception}')
+    
+    # Renames current log file.
+    def __renameLog(self) -> None:
+        """Method that renames the current log, please only use this if you have some idea of what you're doing."""
+        
+        # Creates new path to file
+        newPath = f'{self.directory}{self.separator}{datetime.now():%m-%d-%Y %H-%M-%S}.log'
+        
+        # Checks if current file path is the same as new file path and renames if not.
+        if self.filePath != newPath:
+            rename(self.filePath, newPath)
+            self.filePath = newPath
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# Python-Logger
-A simple logging script that can help you log errors, bugs, or normal functions into a .log file.
+## About aLogger.py ##
+This is a simple logging script that can be used to help debug programs by giving you an easy way to create and append to logs.
+
+## Getting Started ##
+To get started, initialize the object while passing in a **folder directory**.
+`logger = Logger(desiredDirectory)`
+
+## Object Methods ##
+
+#### Logging Low Importance ####
+`logger.low(message)`
+This will write to the current log your message as low importance.
+
+**Output to current log:**
+> {currentDateTime} | LOW | {message}
+
+
+#### Logging Medium Importance ####
+`logger.medium(message)`
+This will write to the current log your message as medium importance.
+
+**Output to current log:**
+> {currentDateTime} | MEDIUM | {message}
+
+#### Logging High Importance ####
+`logger.high(message)`
+This will write to the current log your message as high importance.
+
+**Output to current log:**
+> {currentDateTime} | HIGH | {message}
+
+#### Logging Custom Status ####
+`logger.log(message, importance)`
+This gives you the option to add a line to the log under any status such as WARNING or BUG. If left empty, it will not show importance.
+
+**Output to current log after passing in importance:**
+> {currentDateTime} | {importance} | {message}
+
+**Output to current log without passing in importance:**
+> {currentDateTime} | {message}
